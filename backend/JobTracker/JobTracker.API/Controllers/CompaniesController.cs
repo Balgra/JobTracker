90e81caaccab6a11497454ac3c7cb4891d38223b
Adelin Chis <adelingabriel08@gmail.com>
2022-04-18 22:02:49 +0300

    Adding companies controller

---
+++ backend/JobTracker/JobTracker.API/Controllers/CompaniesController.cs
@@ -0,0 +1,46 @@
+﻿using JobTracker.Core.Database;
+using JobTracker.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTracker.API.Controllers
+{
+    [ApiController]
+    [Route("companies")]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CompaniesController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCompany(string companyName)
+        {
+            if (companyName.Length < 5)
+                return BadRequest("Invalid name! Less than 5 characters!");
+
+            var existingCompany = await _dbContext.Companies.FirstOrDefaultAsync(p => p.CompanyName == companyName);
+
+            if (existingCompany != null)
+                return BadRequest("Invalid name! Company already exist!");
+
+            var company = new Company() { CompanyName = companyName };
+
+            var companyEntry = await _dbContext.Companies.AddAsync(company);
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(company);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await _dbContext.Companies.ToListAsync();
+            return Ok(companies);
+        }
+    }
+}
--- backend/JobTracker/JobTracker.API/Program.cs
+++ backend/JobTracker/JobTracker.API/Program.cs
@@ -5,7 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,7 +15,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(
         options => options.UseSqlServer(builder.Configuration.GetConnectionString("JobTracker"), b => b.MigrationsAssembly("JobTracker.API")));
 
-
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
